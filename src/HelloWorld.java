ec129852f9d16cda79c2455a7798664edb575dbb
muliarob <muliar.o.b@gmail.com>
2018-08-22 11:53:40 +0300

    3rd commit

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,9 @@
 public class HelloWorld {
-    public static void main(String [] args)
+    public static void main(String[] args)
     {
-        System.out.println("Hello, world!");
+        //String s = "Hello";
+
+
+        //System.out.println(s);
     }
 }
---
+++ src/Strings.java
@@ -0,0 +1,10 @@
+public class Strings {
+    public static void main(String[] args)
+    {
+        String s = "Hello";
+        String space = " ";
+        String name = "Bob";
+
+        System.out.println(s+space+name);
+    }
+}
